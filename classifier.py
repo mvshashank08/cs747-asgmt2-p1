d30119e62c9e5f9b4990d20f259b294c9946dd5f
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-17 19:04:35 +0000

    modified architecture

--- classifier.py
+++ classifier.py
@@ -10,22 +10,25 @@ NUM_CLASSES = 21
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 3)
+        self.conv1 = nn.Conv2d(3, 64, 5)
+        self.conv1_bn = nn.BatchNorm2d(64)
         self.conv2 = nn.Conv2d(64, 128, 3)
-        self.conv3 = nn.Conv2d(128, 156, 3)
+        self.conv2_bn = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 128, 3)
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 1, 3)
+        self.conv4_bn = nn.BatchNorm2d(1)
         self.pool = nn.MaxPool2d(2, 2)
-        
-        self.fc1 = nn.Linear(16 * 26 * 26, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, NUM_CLASSES)
+        self.fc1 = nn.Linear(1 * 53 * 53, NUM_CLASSES)
+        self.fc1_bn = nn.BatchNorm1d(NUM_CLASSES)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(x.size()[0], 16 * 26 * 26)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.leaky_relu(self.conv1_bn(self.conv1(x)))
+        x = self.pool(F.leaky_relu(self.conv2_bn(self.conv2(x))))
+        x = F.leaky_relu(self.conv3_bn(self.conv3(x)))
+        x = self.pool(F.leaky_relu(self.conv4_bn(self.conv4(x))))
+        x = x.view(x.size()[0], 1 * 53 * 53)
+        x = self.fc1(x)
         return x
+
 
