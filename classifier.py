d168e04214e80caa2727818b027791ac48562d74
mvshashank08 <mvshashank08@gmail.com>
2020-03-18 05:56:19 +0000

    stacked more layers, added lr scheduler

--- classifier.py
+++ classifier.py
@@ -10,21 +10,24 @@ NUM_CLASSES = 21
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5)
-        self.conv2 = nn.Conv2d(64, 32, 3)
-        self.conv3 = nn.Conv2d(32, 16, 3)
+        self.conv1 = nn.Conv2d(3, 256, 3)
+        self.conv2 = nn.Conv2d(256, 128, 3)
+        self.conv3 = nn.Conv2d(128, 64, 3)
+        self.conv4 = nn.Conv2d(64, 32, 3, padding=1)
+        self.conv5 = nn.Conv2d(32, 16, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 26 * 26, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, NUM_CLASSES)
+        self.fc1 = nn.Linear(16 * 13 * 13, 256)
+        self.fc2 = nn.Linear(256, NUM_CLASSES)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(x.size()[0], 16 * 26 * 26)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = F.relu(self.conv5(x))
+#         print(x.size())
+        x = x.view(x.size()[0], 16 * 13 * 13)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
