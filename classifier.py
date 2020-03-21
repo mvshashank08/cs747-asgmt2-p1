9bc346accc09777f584d0e3cc455e17c6a24c1f9
mvshashank08 <mvshashank08@gmail.com>
2020-03-21 22:12:45 +0000

    tested config

--- classifier.py
+++ classifier.py
@@ -14,11 +14,11 @@ class Classifier(nn.Module):
         nn.init.xavier_uniform_(self.conv1.weight)
         self.conv1_bn = nn.BatchNorm2d(64)
         
-        self.conv2 = nn.Conv2d(64, 64, 3)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
         nn.init.xavier_uniform_(self.conv2.weight)
         self.conv2_bn = nn.BatchNorm2d(64)
         
-        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
         nn.init.xavier_uniform_(self.conv3.weight)
         self.conv3_bn = nn.BatchNorm2d(128)
         
@@ -32,7 +32,7 @@ class Classifier(nn.Module):
         
         self.pool = nn.MaxPool2d(3, 3)
         
-        self.fc1 = nn.Linear(128 * 21 * 21, 1024)
+        self.fc1 = nn.Linear(128 * 5 * 5, 1024)
         nn.init.xavier_uniform_(self.fc1.weight)
         self.fc1_bn = nn.BatchNorm1d(1024)
         
@@ -42,20 +42,19 @@ class Classifier(nn.Module):
         
         self.fc3 = nn.Linear(512, NUM_CLASSES)
         nn.init.xavier_uniform_(self.fc3.weight)
-        self.dropout = nn.Dropout2d(p = 0.1)
+        self.dropout = nn.Dropout(p = 0.4)
 
     def forward(self, x):
-        p = 0.5
-        x = self.conv1_bn(F.relu(self.conv1_bn(self.conv1(x))))
-        x = self.pool(self.conv2_bn(F.relu(self.conv2_bn(self.conv2(x)))))
-        x = self.conv3_bn(F.relu(self.conv3_bn(self.conv3(x))))
-        x = self.pool(self.conv4_bn(F.relu(self.conv4_bn(self.conv4(x)))))
-        x = self.conv5_bn(F.relu(self.conv5_bn(self.conv5(x))))
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2_bn(self.conv2(x))))
+        x = self.pool(F.relu(self.conv3_bn(self.conv3(x))))
+        x = self.pool(F.relu(self.conv4_bn(self.conv4(x))))
+        x = F.relu(self.conv5_bn(self.conv5(x)))
 #         print(x.size())
-        x = x.view(x.size()[0], 128 * 21 * 21)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = x.view(x.size()[0], 128 * 5 * 5)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
         return x
 
 
