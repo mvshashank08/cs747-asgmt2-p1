86eb6f04a4b9855537d555d2346bc69a89a59afd
mvshashank08 <mvshashank08@gmail.com>
2020-03-21 04:45:57 +0000

    classic cnn

--- classifier.py
+++ classifier.py
@@ -10,42 +10,52 @@ NUM_CLASSES = 21
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 3, padding=2)
+        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
+        nn.init.xavier_uniform_(self.conv1.weight)
         self.conv1_bn = nn.BatchNorm2d(64)
         
-        self.conv2 = nn.Conv2d(64, 64, 3, padding=2)
+        self.conv2 = nn.Conv2d(64, 64, 3)
+        nn.init.xavier_uniform_(self.conv2.weight)
         self.conv2_bn = nn.BatchNorm2d(64)
         
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        nn.init.xavier_uniform_(self.conv3.weight)
         self.conv3_bn = nn.BatchNorm2d(128)
         
-        self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, 3)
+        nn.init.xavier_uniform_(self.conv4.weight)
         self.conv4_bn = nn.BatchNorm2d(128)
         
-        self.conv5 = nn.Conv2d(128, 256, 3, padding=1)
-        self.conv5_bn = nn.BatchNorm2d(256)
+        self.conv5 = nn.Conv2d(128, 128, 3)
+        nn.init.xavier_uniform_(self.conv5.weight)
+        self.conv5_bn = nn.BatchNorm2d(128)
         
-        self.conv6 = nn.Conv2d(256, 256, 3, padding=1)
-        self.conv6_bn = nn.BatchNorm2d(256)
+        self.pool = nn.MaxPool2d(3, 3)
         
-        self.conv7 = nn.Conv2d(256, 1, 1)
-        self.conv7 = nn.BatchNorm2d(1)
+        self.fc1 = nn.Linear(128 * 21 * 21, 1024)
+        nn.init.xavier_uniform_(self.fc1.weight)
+        self.fc1_bn = nn.BatchNorm1d(1024)
         
-#         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(1 * 256 * 256, NUM_CLASSES)
-        self.fc1_bn = nn.BatchNorm1d(NUM_CLASSES)
+        self.fc2 = nn.Linear(1024, 512)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        self.fc2_bn = nn.BatchNorm1d(512)
+        
+        self.fc3 = nn.Linear(512, NUM_CLASSES)
+        nn.init.xavier_uniform_(self.fc3.weight)
+        self.dropout = nn.Dropout2d(p = 0.1)
 
     def forward(self, x):
+        p = 0.5
         x = self.conv1_bn(F.relu(self.conv1_bn(self.conv1(x))))
-        x = self.conv2_bn(F.relu(self.conv2_bn(self.conv2(x))))
+        x = self.pool(self.conv2_bn(F.relu(self.conv2_bn(self.conv2(x)))))
         x = self.conv3_bn(F.relu(self.conv3_bn(self.conv3(x))))
-        x = self.conv4_bn(F.relu(self.conv4_bn(self.conv4(x))))
+        x = self.pool(self.conv4_bn(F.relu(self.conv4_bn(self.conv4(x)))))
         x = self.conv5_bn(F.relu(self.conv5_bn(self.conv5(x))))
-        x = self.conv6_bn(F.relu(self.conv6_bn(self.conv6(x))))
-        x = self.conv7_bn(F.relu(self.conv7_bn(self.conv7(x))))
-        
-        x = x.view(x.size()[0], 1 * 256 * 256)
-        x = self.fc1_bn(self.fc1(x))
+#         print(x.size())
+        x = x.view(x.size()[0], 128 * 21 * 21)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         return x
 
 
