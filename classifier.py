bacb1b627621d5756137a7bcfae3b733b9926746
mvshashank08 <mvshashank08@gmail.com>
2020-03-22 22:17:23 +0000

    new test

--- classifier.py
+++ classifier.py
@@ -11,45 +11,37 @@ class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
-        nn.init.xavier_uniform_(self.conv1.weight)
         self.conv1_bn = nn.BatchNorm2d(64)
         
-        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
-        nn.init.xavier_uniform_(self.conv2.weight)
-        self.conv2_bn = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_bn = nn.BatchNorm2d(128)
         
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
-        nn.init.xavier_uniform_(self.conv3.weight)
-        self.conv3_bn = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 152, 3, padding=1)
+        self.conv3_bn = nn.BatchNorm2d(152)
         
-        self.conv4 = nn.Conv2d(128, 128, 3)
-        nn.init.xavier_uniform_(self.conv4.weight)
-        self.conv4_bn = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(152, 192, 3)
+        self.conv4_bn = nn.BatchNorm2d(192)
         
-        self.conv5 = nn.Conv2d(128, 128, 3)
-        nn.init.xavier_uniform_(self.conv5.weight)
+        self.conv5 = nn.Conv2d(192, 128, 3)
         self.conv5_bn = nn.BatchNorm2d(128)
         
         self.pool = nn.MaxPool2d(3, 3)
         
         self.fc1 = nn.Linear(128 * 5 * 5, 2048)
-        nn.init.xavier_uniform_(self.fc1.weight)
         self.fc1_bn = nn.BatchNorm1d(2048)
         
         self.fc2 = nn.Linear(2048, 1024)
-        nn.init.xavier_uniform_(self.fc2.weight)
         self.fc2_bn = nn.BatchNorm1d(1024)
         
         self.fc3 = nn.Linear(1024, NUM_CLASSES)
-        nn.init.xavier_uniform_(self.fc3.weight)
         self.dropout = nn.Dropout(p = 0.4)
 
     def forward(self, x):
-        x = F.relu(self.conv1_bn(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2_bn(self.conv2(x))))
-        x = self.pool(F.relu(self.conv3_bn(self.conv3(x))))
-        x = self.pool(F.relu(self.conv4_bn(self.conv4(x))))
-        x = F.relu(self.conv5_bn(self.conv5(x)))
+        x = self.conv1_bn(F.relu(self.conv1(x)))
+        x = self.pool(self.conv2_bn(F.relu(self.conv2(x))))
+        x = self.pool(self.conv3_bn(F.relu(self.conv3(x))))
+        x = self.pool(self.conv4_bn(F.relu(self.conv4(x))))
+        x = self.conv5_bn(F.relu(self.conv5(x)))
 #         print(x.size())
         x = x.view(x.size()[0], 128 * 5 * 5)
         x = self.dropout(F.relu(self.fc1_bn(self.fc1(x))))
