59c206c17ef906f0e8b356d0ca25964ea62d6f52
mvshashank08 <mvshashank08@gmail.com>
2020-03-23 06:36:40 +0000

    mAP 0.2235

--- classifier.py
+++ classifier.py
@@ -16,15 +16,15 @@ class Classifier(nn.Module):
         self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
         self.conv2_bn = nn.BatchNorm2d(128)
         
-        self.conv3 = nn.Conv2d(128, 152, 3, padding=1)
-        self.conv3_bn = nn.BatchNorm2d(152)
+        self.conv3 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_bn = nn.BatchNorm2d(256)
         
 #         self.conv4 = nn.Conv2d(152, 192, 3)
 #         self.conv4_bn = nn.BatchNorm2d(192)
         
         self.pool = nn.MaxPool2d(2, 2)
         
-        self.fc1 = nn.Linear(152 * 28 * 28, 256)
+        self.fc1 = nn.Linear(256 * 28 * 28, 256)
         self.fc1_bn = nn.BatchNorm1d(256)
         
 #         self.fc2 = nn.Linear(512, 256)
@@ -39,7 +39,7 @@ class Classifier(nn.Module):
         x = self.pool(self.conv3_bn(F.relu(self.conv3(x))))
 #         x = self.pool(self.conv4_bn(F.relu(self.conv4(x))))
 #         print(x.size())
-        x = x.view(x.size()[0], 152 * 28 * 28)
+        x = x.view(x.size()[0], 256 * 28 * 28)
         x = self.dropout(self.fc1_bn(F.relu(self.fc1(x))))
 #         print(x.size())
 #         x = self.dropout(self.fc2_bn(F.relu(self.fc2(x))))
